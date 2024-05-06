e9547cf12c9f58b39989c4d651db96cffd03f55c
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-05-06 16:50:05 -0400

    subindo

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bem vindo ao meu portfólio!!</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default HomePage;
